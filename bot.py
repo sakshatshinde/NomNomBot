5e1b40f5d3e75b783f5f2404b12724fa11aee32a
Sakshat Shinde <shindesakshat@gmail.com>
2019-12-22 17:02:59 +0530

    Added commands
    Added new command and changed the way bot interacts.

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import os, discord, random
 from dotenv import load_dotenv
-from content.quotes import nom_nom_quotes
+from content.quotes import *
 from discord.ext import commands
 
 
@@ -8,21 +8,22 @@ from discord.ext import commands
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-client = discord.Client()
-#######bot = commands.Bot(command_prefix=">")
-#on_ready() event handler, which handles the event when the Client has established a connection to Discord
-@client.event
-async def on_ready():
-    print(f'{client.user} connection established to Discord!')
+#client = discord.Client()
+bot = commands.Bot(command_prefix=">", description='A tasty bot for tasty food')
 
-@client.event   #sending a welcome msg via dm
+#on_ready() event handler, which handles the event when the Client has established a connection to Discord
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} connection established to Discord!')
+
+@bot.event   #sending a welcome msg via dm
 async def on_member_join(member):
     await member.create_dm()    
     await member.dm_channel.send(
         f'Hieee {member.name}, welcome to my Discord server!'
     )
 
-@client.event   #error handling
+@bot.event   #error handling
 async def on_error(event, *args, **kwargs):
     with open('err.log', 'a') as f:
         if event == 'on_message':
@@ -30,30 +31,43 @@ async def on_error(event, *args, **kwargs):
         else:
             pass
 
-@client.event 
-async def on_message(message):
-    if(message.author == client.user):   
-        return                          #Preventing recursive hole of bot replying to itself 
-    
-    if(message.content == "yum"):
-        response = random.choice(nom_nom_quotes)
-        await message.channel.send(response)
-    elif(message.content == "Yum"):
-        response = random.choice(nom_nom_quotes)
-        await message.channel.send(response)
-    elif message.content == 'raise-exception':
-        raise discord.DiscordException
 
-'''@bot.command(name="99")
+@bot.command(name = "99") 
 async def nine_nine(ctx):
     response = random.choice(brooklyn_99_quotes)
     await ctx.send(response)
 
-@bot.command(name="yum")
+@bot.command(name = "yum")
 async def nom_nome(ctx):
-    response = random.choice(nom_nom_quotes)
-    await ctx.send(response)'''
+    try:
+        response = random.choice(nom_nom_quotes)
+        await ctx.send(response)
+    except:
+        raise discord.DiscordException
 
-client.run(TOKEN)
-#bot.run(TOKEN)
+@bot.command(mame = "info")
+async def info(ctx):
+    embed = discord.Embed(title="NomNom", description="A discord bot dedicated to yummy food", color=0xeee657)
+
+    # give info about you here
+    embed.add_field(name="Maker", value="ScreX")
+
+    # Shows the number of servers the bot is member of.
+    embed.add_field(name="Server count", value=f"{len(bot.guilds)}")
+
+    # give users a link to invite thsi bot to their server
+    embed.add_field(name="Invite", value="[Invite link](https://discordapp.com/api/oauth2/authorize?client_id=289205523390267402&permissions=8&scope=bot)")
+    await ctx.send(embed = embed)
+
+@bot.command(name = "toni")
+async def toni(ctx):
+    #embed = discord.Embed()
+    #embed.set_image(url = "https://imgur.com/gallery/iX06rQg")
+    try:
+        await ctx.send("https://imgur.com/gallery/iX06rQg")
+    except:
+        pass
+
+#client.run(TOKEN)
+bot.run(TOKEN)
 
