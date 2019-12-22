54cc2d9196ee0c12cb9ac3ef45af0ce7d0b7eb6c
Sakshat Shinde <shindesakshat@gmail.com>
2019-12-22 16:00:34 +0530

    Added food quotes
    Added some interesting food quotes for the bot to respond when someone is hungry.

--- bot.py
+++ bot.py
@@ -1,14 +1,58 @@
-import os, discord
+import os, discord, random
 from dotenv import load_dotenv
+from content.quotes import *
+from discord.ext import commands
 
+#Storing API keys safely.
 load_dotenv()
-token = os.getenv('DISCORD_TOKEN')
+TOKEN = os.getenv('DISCORD_TOKEN')
 
 client = discord.Client()
-
+bot = commands.Bot(command_prefix=">")
 #on_ready() event handler, which handles the event when the Client has established a connection to Discord
 @client.event
 async def on_ready():
     print(f'{client.user} connection established to Discord!')
 
-client.run(token)
+@client.event   #sending a welcome msg via dm
+async def on_member_join(member):
+    await member.create_dm()    
+    await member.dm_channel.send(
+        f'Hieee {member.name}, welcome to my Discord server!'
+    )
+
+@client.event   #error handling
+async def on_error(event, *args, **kwargs):
+    with open('err.log', 'a') as f:
+        if event == 'on_message':
+            f.write(f'Unhandled message: {args[0]}\n')
+        else:
+            pass
+
+@client.event 
+async def on_message(message):
+    if(message.author == client.user):   
+        return                          #Preventing recursive hole of bot replying to itself 
+    
+    if(message.content == "yum"):
+        response = random.choice(nom_nom_quotes)
+        await message.channel.send(response)
+    elif(message.content == "Yum"):
+        response = random.choice(nom_nom_quotes)
+        await message.channel.send(response)
+    elif message.content == 'raise-exception':
+        raise discord.DiscordException
+
+'''@bot.command(name="99")
+async def nine_nine(ctx):
+    response = random.choice(brooklyn_99_quotes)
+    await ctx.send(response)
+
+@bot.command(name="yum")
+async def nom_nome(ctx):
+    response = random.choice(nom_nom_quotes)
+    await ctx.send(response)'''
+
+client.run(TOKEN)
+#bot.run(TOKEN)
+
---
+++ content/quotes.py
@@ -0,0 +1,20 @@
+brooklyn_99_quotes = [
+        'I\'m the human form of the 💯 emoji.',
+        'Bingpot!',
+        (
+            'Cool. Cool cool cool cool cool cool cool, '
+            'no doubt no doubt no doubt no doubt.'
+        ),
+    ]
+
+nom_nom_quotes = [
+    'All you need is love. But a little chocolate now and then doesn\'t hurt',
+    'If more of us valued food and cheer and song above hoarded gold, it would be a merrier world.',
+    'I love you like a fat kid loves cake!',
+    'One cannot think well, love well, sleep well, if one has not dined well',
+    'Ask not what you can do for your country. Ask what’s for lunch',
+    'After a good dinner one can forgive anybody, even one\'s own relations.',
+    'There are people in the world so hungry, that God cannot appear to them except in the form of bread.',
+    'There is no love sincerer than the love of food',
+    'What I say is that, if a man really likes potatoes, he must be a pretty decent sort of fellow.'
+]
