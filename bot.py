ab189150c581d575cdad445290eb359ac9c7be7c
Sakshat Shinde <shindesakshat@gmail.com>
2019-12-23 23:51:05 +0530

    Added status

--- bot.py
+++ bot.py
@@ -3,7 +3,6 @@ from dotenv import load_dotenv
 from content.quotes import *
 from discord.ext import commands
 
-
 #Storing API keys safely.
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -15,6 +14,7 @@ bot = commands.Bot(command_prefix=">", description='A tasty bot for tasty food')
 @bot.event
 async def on_ready():
     print(f'{bot.user.name} connection established to Discord!')
+    await bot.change_presence(status=discord.Status.online, activity=discord.Game("with 🍕🍟🥨🌮"))
 
 @bot.event   #sending a welcome msg via dm
 async def on_member_join(member):
@@ -22,6 +22,7 @@ async def on_member_join(member):
     await member.dm_channel.send(
         f'Hieee {member.name}, welcome to my Discord server!'
     )
+
 
 @bot.event   #error handling
 async def on_error(event, *args, **kwargs):
